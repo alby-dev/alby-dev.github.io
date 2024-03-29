352db4cba1f79c3e7dfe067a5c33101fb4149e18
Alby DEV <153741499+alby-dev@users.noreply.github.com>
2024-03-29 17:45:10 +0100

    Update main.js

--- themes/hugoplate/assets/js/main.js
+++ themes/hugoplate/assets/js/main.js
@@ -1,4 +1,59 @@
+var currentURL = window.location.href;
+// Nuovo dominio di destinazione
+var newDomain = "https://albertoreineri.it";
+
+if(currentURL == 'https://alby-dev.github.io/blog/'){
+  window.location.href = 'https://albertoreineri.it/blog/';
+}
+
+if(currentURL == 'https://alby-dev.github.io/guide/'){
+  window.location.href = 'https://albertoreineri.it/blog/';
+}
+// Creare il nuovo URL mantenendo il percorso relativo
+var newURL = newDomain + window.location.pathname + window.location.search + window.location.hash;
+
+// Fare il redirect verso il nuovo URL
+window.location.href = newURL;
+
 // main script
+(function () {
+  "use strict";
+
+  // Dropdown Menu Toggler For Mobile
+  // ----------------------------------------
+  const dropdownMenuToggler = document.querySelectorAll(
+    ".nav-dropdown > .nav-link",
+  );
+
+  dropdownMenuToggler.forEach((toggler) => {
+    toggler?.addEventListener("click", (e) => {
+      e.target.parentElement.classList.toggle("active");
+    });
+  });
+
+  // Testimonial Slider
+  // ----------------------------------------
+  new Swiper(".testimonial-slider", {
+    spaceBetween: 24,
+    loop: true,
+    pagination: {
+      el: ".testimonial-slider-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+    },
+  });
+})();
+
+
+      // main script
 (function () {
   "use strict";
 
